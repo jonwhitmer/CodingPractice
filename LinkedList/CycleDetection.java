530ac0421588df35273ca57e4caefec0c77bb66e
jonwhitmer <jonwhitmer23@gmail.com>
2025-06-15 17:29:24 -0400

    Cycle Detection

---
+++ LinkedList/CycleDetection.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class CycleDetection 
+{
+
+    public boolean hasCycle(ListNode head)
+    {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null)
+        {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if (slowPointer == fastPointer)
+            {
+                return true; // Cycle detected
+            }
+        }
+
+        return false;
+    }
+
+
+}
+
+
+
+/* This is O(n) space complexity
+ *     public boolean hasCycle(ListNode head) 
+    {
+        Set<ListNode> visitedSet = new HashSet<>();
+        ListNode currentNode = head;
+
+        while (currentNode != null)
+        {
+            if (!visitedSet.contains(currentNode))
+            {
+                visitedSet.add(currentNode);
+                currentNode = currentNode.next;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+ */
